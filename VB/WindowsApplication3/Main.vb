e9517c189490e423f04af6f024b1a587d0f5d9c0
DevExpressExampleBot <fakeEmail>
2019-04-12 13:09:31 +0300

    Source auto update [skip ci]

--- VB/WindowsApplication3/Main.vb
+++ VB/WindowsApplication3/Main.vb
@@ -5,22 +5,22 @@ Imports DevExpress.XtraEditors
 Imports DXSample.Data
 
 Namespace DXSample
-    Partial Public Class Main
-        Inherits XtraForm
+	Partial Public Class Main
+		Inherits XtraForm
 
-        Private provider As FilterNameProvider
-        Public Sub New()
-            InitializeComponent()
-            recordBindingSource.DataSource = DataHelper.GetData(10)
-            provider = New FilterNameProvider(gridView1) With {.AllowSettingFilterNames = True}
-        End Sub
-        Private filePath As String = "Layout.xml"
-        Private Sub OnSaveLayoutButtonClick(ByVal sender As Object, ByVal e As DevExpress.XtraBars.ItemClickEventArgs) Handles barButtonItem4.ItemClick
-            gridView1.SaveLayoutToXmlEx(provider, filePath)
-        End Sub
+		Private provider As FilterNameProvider
+		Public Sub New()
+			InitializeComponent()
+			recordBindingSource.DataSource = DataHelper.GetData(10)
+			provider = New FilterNameProvider(gridView1) With {.AllowSettingFilterNames = True}
+		End Sub
+		Private filePath As String = "Layout.xml"
+		Private Sub OnSaveLayoutButtonClick(ByVal sender As Object, ByVal e As DevExpress.XtraBars.ItemClickEventArgs) Handles barButtonItem4.ItemClick
+			gridView1.SaveLayoutToXmlEx(provider, filePath)
+		End Sub
 
-        Private Sub OnRestoreLayoutButtonClick(ByVal sender As Object, ByVal e As DevExpress.XtraBars.ItemClickEventArgs) Handles barButtonItem5.ItemClick
-            gridView1.RestoreLayoutFromXmlEx(provider, filePath)
-        End Sub
-    End Class
+		Private Sub OnRestoreLayoutButtonClick(ByVal sender As Object, ByVal e As DevExpress.XtraBars.ItemClickEventArgs) Handles barButtonItem5.ItemClick
+			gridView1.RestoreLayoutFromXmlEx(provider, filePath)
+		End Sub
+	End Class
 End Namespace
--- VB/WindowsApplication3/My Project/Settings.Designer.vb
+++ VB/WindowsApplication3/My Project/Settings.Designer.vb
@@ -11,23 +11,27 @@
 Namespace My
 
 
-    <Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(), Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")> _
-    Friend NotInheritable Partial Class Settings
-        Inherits System.Configuration.ApplicationSettingsBase
+	<Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute()>
+	<Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")>
+	Friend NotInheritable Partial Class Settings
+		Inherits System.Configuration.ApplicationSettingsBase
 
-        Private Shared defaultInstance As Settings = (CType(Global.System.Configuration.ApplicationSettingsBase.Synchronized(New Settings()), Settings))
+		Private Shared defaultInstance As Settings = (CType(Global.System.Configuration.ApplicationSettingsBase.Synchronized(New Settings()), Settings))
 
-        Public Shared ReadOnly Property [Default]() As Settings
-            Get
-                Return defaultInstance
-            End Get
-        End Property
+		Public Shared ReadOnly Property [Default]() As Settings
+			Get
+				Return defaultInstance
+			End Get
+		End Property
 
-        <Global.System.Configuration.ApplicationScopedSettingAttribute(), Global.System.Diagnostics.DebuggerNonUserCodeAttribute(), Global.System.Configuration.SpecialSettingAttribute(Global.System.Configuration.SpecialSetting.ConnectionString), Global.System.Configuration.DefaultSettingValueAttribute("Data Source=.\SQLEXPRESS;Initial Catalog=northwind;Integrated Security=True")> _
-        Public ReadOnly Property northwindConnectionString() As String
-            Get
-                Return (DirectCast(Me("northwindConnectionString"), String))
-            End Get
-        End Property
-    End Class
+		<Global.System.Configuration.ApplicationScopedSettingAttribute()>
+		<Global.System.Diagnostics.DebuggerNonUserCodeAttribute()>
+		<Global.System.Configuration.SpecialSettingAttribute(Global.System.Configuration.SpecialSetting.ConnectionString)>
+		<Global.System.Configuration.DefaultSettingValueAttribute("Data Source=.\SQLEXPRESS;Initial Catalog=northwind;Integrated Security=True")>
+		Public ReadOnly Property northwindConnectionString() As String
+			Get
+				Return (DirectCast(Me("northwindConnectionString"), String))
+			End Get
+		End Property
+	End Class
 End Namespace
